79b8aaa537d9087c5ee982b1d9950d71150c7749
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-21 18:16:16 +0200

    Use rclone vault engine

--- Elephant/back/app/runtime/elephantRuntime.js
+++ Elephant/back/app/runtime/elephantRuntime.js
@@ -1,7 +1,7 @@
-import { GitSyncEngine } from '../sync/GitSyncEngine'
+import { RcloneVaultEngine } from '../sync/rcloneVaultEngine.js'
 import { ModelRuntime } from '../modelRuntime'
 import { ProgramRuntime } from '../programRuntime'
 
-export const syncEngine = new GitSyncEngine()
+export const syncEngine = new RcloneVaultEngine()
 export const modelRuntime = new ModelRuntime()
 export const programRuntime = new ProgramRuntime()

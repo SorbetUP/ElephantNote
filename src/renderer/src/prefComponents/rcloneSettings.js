0b22ece8408d160f1be8693a4f2393004347bfc4
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-21 18:22:54 +0200

    Add basic state to rclone settings

--- src/renderer/src/prefComponents/rcloneSettings.js
+++ src/renderer/src/prefComponents/rcloneSettings.js
@@ -2,11 +2,20 @@ import { h } from 'vue'
 
 export default {
   name: 'RcloneSettings',
+  data: () => ({
+    remotePath: '',
+    statusLabel: 'Not configured'
+  }),
   render() {
     return h('div', { class: 'pref-rclone' }, [
       h('h4', 'Sync'),
       h('p', 'Rclone vault transfer'),
-      h('p', 'Use sync.run with init.remotePath and snapshot to run bisync.')
+      h('p', `Status: ${this.statusLabel}`),
+      h('input', {
+        value: this.remotePath,
+        placeholder: 'remote:ElephantVault',
+        onInput: (event) => { this.remotePath = event.target.value }
+      })
     ])
   }
 }

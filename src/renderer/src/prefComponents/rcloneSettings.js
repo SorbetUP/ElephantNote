d2fae75e8122d8d430c71ceb97f96c5477787b41
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-21 18:21:54 +0200

    Render basic rclone settings component

--- src/renderer/src/prefComponents/rcloneSettings.js
+++ src/renderer/src/prefComponents/rcloneSettings.js
@@ -1,3 +1,12 @@
+import { h } from 'vue'
+
 export default {
-  name: 'RcloneSettings'
+  name: 'RcloneSettings',
+  render() {
+    return h('div', { class: 'pref-rclone' }, [
+      h('h4', 'Sync'),
+      h('p', 'Rclone vault transfer'),
+      h('p', 'Use sync.run with init.remotePath and snapshot to run bisync.')
+    ])
+  }
 }

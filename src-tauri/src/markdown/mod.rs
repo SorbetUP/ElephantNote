aaebba52231abda81e5e024574caf8044b7d6dda
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 14:20:42 +0200

    fix tauri switch markdown tests to parser v3

--- src-tauri/src/markdown/mod.rs
+++ src-tauri/src/markdown/mod.rs
@@ -1,10 +1,7 @@
 pub mod commands;
-pub mod parser_v2;
+pub mod parser_v3;
 pub mod renderer;
 pub mod types;
 
-#[cfg(test)]
-mod parser_v2_extra_tests;
-
-pub use parser_v2::parse_markdown_document;
+pub use parser_v3::parse_markdown_document;
 pub use renderer::{render_html, render_plain_text};

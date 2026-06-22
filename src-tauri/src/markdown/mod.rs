37cc90bed7f77c7b79c32df0e45b3a56f9819416
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 14:10:25 +0200

    feat tauri switch markdown parser implementation

--- src-tauri/src/markdown/mod.rs
+++ src-tauri/src/markdown/mod.rs
@@ -1,7 +1,7 @@
 pub mod commands;
-pub mod parser;
+pub mod parser_v2;
 pub mod renderer;
 pub mod types;
 
-pub use parser::parse_markdown_document;
+pub use parser_v2::parse_markdown_document;
 pub use renderer::{render_html, render_plain_text};

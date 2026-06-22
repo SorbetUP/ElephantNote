3ecdf39c69e59f0c2ffecf3a0b2d9b9323ae5aa6
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 14:43:07 +0200

    feat tauri expose Muya compatibility commands

--- src-tauri/src/markdown/commands.rs
+++ src-tauri/src/markdown/commands.rs
@@ -1,6 +1,7 @@
 use serde_json::{json, Value};
 
 use super::{parse_markdown_document, render_html, render_plain_text};
+use super::muya_compat::{parse_muya_document, render_muya_html, tokenize_muya};
 use super::parser_v4::{extract_images, extract_links, parse_blocks, split_frontmatter};
 
 #[tauri::command]
@@ -28,4 +29,19 @@ pub fn tauri_markdown_extract_frontmatter(markdown: String) -> Value {
 #[tauri::command]
 pub fn tauri_markdown_extract_links(markdown: String) -> Value {
   json!({ "links": extract_links(&markdown), "images": extract_images(&markdown) })
+}
+
+#[tauri::command]
+pub fn tauri_muya_parse(markdown: String) -> Value {
+  parse_muya_document(&markdown)
+}
+
+#[tauri::command]
+pub fn tauri_muya_render_html(markdown: String) -> Value {
+  json!({ "html": render_muya_html(&markdown) })
+}
+
+#[tauri::command]
+pub fn tauri_muya_tokens(markdown: String) -> Value {
+  json!({ "tokens": tokenize_muya(&markdown) })
 }

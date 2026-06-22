720e8d0ae13f0d83a1da694b449140444726f462
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 14:23:20 +0200

    test tauri enable markdown parity fixture tests

--- src-tauri/src/markdown/mod.rs
+++ src-tauri/src/markdown/mod.rs
@@ -3,5 +3,8 @@ pub mod parser_v3;
 pub mod renderer;
 pub mod types;
 
+#[cfg(test)]
+mod parity_tests;
+
 pub use parser_v3::parse_markdown_document;
 pub use renderer::{render_html, render_plain_text};

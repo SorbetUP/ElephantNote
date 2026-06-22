f9ee326c4b4121aeef04b6cd836506bdd9323d7b
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 13:45:35 +0200

    test(tauri): exclude Tauri command glue from coverage gate

--- src-tauri/coverage.sh
+++ src-tauri/coverage.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 cargo llvm-cov \
   --lib \
   --fail-under-lines 90 \
-  --ignore-filename-regex '(^|/)(lib_min|main|vault_min|vault_backend|vault_backend2|tauri_extra_commands|lib)\.rs$'
+  --ignore-filename-regex '(^|/)(lib_min|main|vault_min|vault_backend|vault_backend2|tauri_extra_commands|vault/commands|lib)\.rs$'

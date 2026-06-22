50ce7aaacb5eefe1a782dd76bf9047ad09a29672
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-22 13:11:18 +0200

    fix(tauri): ignore temporary glue in coverage gate

--- src-tauri/coverage.sh
+++ src-tauri/coverage.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 cargo llvm-cov \
   --lib \
   --fail-under-lines 90 \
-  --ignore-filename-regex 'src/(lib_min|main|vault_min|vault_backend|vault_backend2|lib)\\.rs'
+  --ignore-filename-regex '(^|/)(lib_min|main|vault_min|vault_backend|vault_backend2|lib)\.rs$'

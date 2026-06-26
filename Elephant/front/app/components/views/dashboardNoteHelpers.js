41dcfe6e78d753fe48a4d1afc9969dc0e3688ae9
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-06-26 02:14:18 +0200

    Store dashboard note in hidden dashboard folder

--- Elephant/front/app/components/views/dashboardNoteHelpers.js
+++ Elephant/front/app/components/views/dashboardNoteHelpers.js
@@ -1,6 +1,6 @@
-export const DASHBOARD_NOTE_DIRECTORY = ''
+export const DASHBOARD_NOTE_DIRECTORY = '.dashboard'
 export const DASHBOARD_NOTE_FILENAME = 'Dashboard.md'
-export const DASHBOARD_NOTE_RELATIVE_PATH = DASHBOARD_NOTE_FILENAME
+export const DASHBOARD_NOTE_RELATIVE_PATH = `${DASHBOARD_NOTE_DIRECTORY}/${DASHBOARD_NOTE_FILENAME}`
 
 export const buildDashboardNoteCreatePayload = () => ({
   relativePath: DASHBOARD_NOTE_DIRECTORY,

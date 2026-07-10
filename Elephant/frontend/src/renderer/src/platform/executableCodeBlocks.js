528b38270e619225af525e9e3beb7cfdd0cdf37f
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-10 10:41:50 +0200

    Activate consolidated executable code runtime

--- Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
+++ Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
@@ -1,7 +1,1 @@
-import { installExecutableCodeBlockChrome } from './executableCodeBlockChrome'
-import { installExecutableCodeBlocks as installRuntime } from './executableCodeBlocksV3'
-
-export const installExecutableCodeBlocks = (target = globalThis) => {
-  const runtime = installRuntime(target)
-  return installExecutableCodeBlockChrome(target, runtime)
-}
+export { installExecutableCodeBlocks } from './executableCodeBlocksV4'

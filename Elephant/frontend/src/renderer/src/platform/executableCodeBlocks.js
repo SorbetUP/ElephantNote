16b57f68c2bf4f057678edc40320587a4be33d78
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-10 13:00:11 +0200

    Install V6 lifecycle guard with inline runtime

--- Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
+++ Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
@@ -1,4 +1,6 @@
-import { installExecutableCodeBlocks } from './executableCodeBlocksV6'
+import { installExecutableCodeBlocks as installRuntime } from './executableCodeBlocksV6'
+import { installExecutableCodeBlocksV6Lifecycle } from './executableCodeBlocksV6Lifecycle'
 import './executableCodeBlocksV6.runtime.css'
 
-export { installExecutableCodeBlocks }
+export const installExecutableCodeBlocks = (target = globalThis) =>
+  installExecutableCodeBlocksV6Lifecycle(installRuntime(target))

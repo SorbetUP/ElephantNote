046a59321a1b78587d118895165218851aa1eff2
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-10 14:45:39 +0200

    feat(muya): mirror Muya cursor and history grouping into Rust

--- Elephant/frontend/src/renderer/src/muya/realMuyaRustAdapter.js
+++ Elephant/frontend/src/renderer/src/muya/realMuyaRustAdapter.js
@@ -1,18 +1,33 @@
 import Muya from '../../../muya/lib'
 import { createRealMuyaRustMirror } from './realMuyaRustMirrorRuntime.js'
 
+const historyIdentity = (history) => {
+  if (!history || typeof history !== 'object') return ''
+  const index = Number.isInteger(history.index) ? history.index : -1
+  const lastEditIndex = Number.isInteger(history.lastEditIndex) ? history.lastEditIndex : -1
+  return `${index}:${lastEditIndex}`
+}
+
 export default class RealMuyaWithRustMirror extends Muya {
   constructor (element, options = {}) {
     super(element, options)
 
+    this.__elephantRustHistoryIdentity = ''
     this.__elephantRustMirror = createRealMuyaRustMirror({
       initialMarkdown: options.markdown || '',
       target: globalThis
     })
 
-    this.__elephantRustChangeListener = ({ markdown } = {}) => {
+    this.__elephantRustChangeListener = ({ markdown, muyaIndexCursor, history } = {}) => {
       if (typeof markdown !== 'string') return
-      this.__elephantRustMirror.sync(markdown, 'muya-change')
+      const nextHistoryIdentity = historyIdentity(history)
+      const continueGroup = nextHistoryIdentity !== '' &&
+        nextHistoryIdentity === this.__elephantRustHistoryIdentity
+      this.__elephantRustHistoryIdentity = nextHistoryIdentity
+      this.__elephantRustMirror.sync(markdown, 'muya-change', {
+        muyaIndexCursor,
+        continueGroup
+      })
     }
 
     this.on('change', this.__elephantRustChangeListener)
@@ -20,7 +35,10 @@ export default class RealMuyaWithRustMirror extends Muya {
 
   setMarkdown (markdown, ...args) {
     const result = super.setMarkdown(markdown, ...args)
-    this.__elephantRustMirror?.sync(markdown, 'set-markdown')
+    this.__elephantRustHistoryIdentity = ''
+    this.__elephantRustMirror?.sync(markdown, 'set-markdown', {
+      continueGroup: false
+    })
     return result
   }
 
@@ -31,6 +49,7 @@ export default class RealMuyaWithRustMirror extends Muya {
     this.__elephantRustMirror?.destroy()
     this.__elephantRustMirror = null
     this.__elephantRustChangeListener = null
+    this.__elephantRustHistoryIdentity = ''
     return super.destroy()
   }
 }

9d9551bc280f675b3d3850072b7cd699444656e3
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-10 03:40:32 +0200

    Wire executable controls into Muya chrome

--- Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
+++ Elephant/frontend/src/renderer/src/platform/executableCodeBlocks.js
@@ -1,1 +1,7 @@
-export { installExecutableCodeBlocks } from './executableCodeBlocksV3'
+import { installExecutableCodeBlockChrome } from './executableCodeBlockChrome'
+import { installExecutableCodeBlocks as installRuntime } from './executableCodeBlocksV3'
+
+export const installExecutableCodeBlocks = (target = globalThis) => {
+  const runtime = installRuntime(target)
+  return installExecutableCodeBlockChrome(target, runtime)
+}

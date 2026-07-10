3d787f3db4b65b9174f969640551c39f922c0a03
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-10 20:30:15 +0200

    Signal Android document-tree synchronization after vault mutations

--- Elephant/frontend/app/services/elephantnoteClient.js
+++ Elephant/frontend/app/services/elephantnoteClient.js
@@ -6,10 +6,37 @@ import {
 import { requireAtomicFeatureApi } from './elephantnoteClient/atomicFeatureApi'
 import { createDomainClients } from './elephantnoteClient/domainClients'
 import { COMPATIBILITY_CALLS } from './elephantnoteClient/compatibilityCalls'
+import { ELEPHANTNOTE_API_ACTIONS as API } from 'common/elephantnote/apiActions'
 
 export { isElephantNoteApiAvailable }
 
-const call = createApiCaller(COMPATIBILITY_CALLS)
+const rawCall = createApiCaller(COMPATIBILITY_CALLS)
+const VAULT_MUTATIONS = new Set([
+  API.NOTES_CREATE,
+  API.NOTES_WRITE,
+  API.FOLDERS_CREATE,
+  API.SIDEBAR_ATTACH,
+  API.SIDEBAR_DETACH,
+  API.ENTRIES_RENAME,
+  API.ENTRIES_MOVE,
+  API.ENTRIES_DELETE,
+  API.WIKI_ACCEPT,
+  API.WIKI_DISMISS,
+  API.CALENDAR_IMPORT_GOOGLE,
+  API.CALENDAR_IMPORT_GOOGLE_FROM_PATH,
+  API.SOURCES_INGEST_URL,
+  API.SOURCES_IMPORT_RSS
+].filter(Boolean))
+
+const call = async (action, payload = {}) => {
+  const result = await rawCall(action, payload)
+  if (VAULT_MUTATIONS.has(action)) {
+    globalThis.window?.dispatchEvent?.(new CustomEvent('elephantnote:vault-mutated', {
+      detail: { action }
+    }))
+  }
+  return result
+}
 
 export const elephantnoteClient = {
   describe: () => requireElephantNoteApi().describe(),

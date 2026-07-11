1bdcc106e3b15b7e75631f6933e912c1be6b153b
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-11 23:15:59 +0200

    feat(addons): register extracted system features as addons

--- Elephant/frontend/src/renderer/src/addons/builtin/index.js
+++ Elephant/frontend/src/renderer/src/addons/builtin/index.js
@@ -1,24 +1,33 @@
 import { addonInspectorAddon } from './addonInspector'
 import { addonProfilesAddon } from './addonProfiles'
 import { calendarAddon } from './calendar'
+import { codexConnectionAddon } from './codexConnection'
 import { dailyNotesAddon } from './dailyNotes'
+import { googleKeepImportAddon } from './googleKeepImport'
 import { quickCaptureAddon } from './quickCapture'
+import { sitesAddon } from './sites'
 import { vaultOverviewAddon } from './vaultOverview'
 
 export {
   addonInspectorAddon,
   addonProfilesAddon,
   calendarAddon,
+  codexConnectionAddon,
   dailyNotesAddon,
+  googleKeepImportAddon,
   quickCaptureAddon,
+  sitesAddon,
   vaultOverviewAddon
 }
 
 export const builtinAddons = [
+  addonProfilesAddon,
+  googleKeepImportAddon,
+  codexConnectionAddon,
+  calendarAddon,
+  sitesAddon,
   dailyNotesAddon,
   quickCaptureAddon,
   vaultOverviewAddon,
-  addonProfilesAddon,
-  calendarAddon,
   addonInspectorAddon
 ]

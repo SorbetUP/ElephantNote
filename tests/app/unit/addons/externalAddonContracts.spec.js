096c4c556097659a6f98796c86097b96e6ca09be
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-12 03:09:11 +0200

    test(addons): include optional editor feature addons

--- tests/app/unit/addons/externalAddonContracts.spec.js
+++ tests/app/unit/addons/externalAddonContracts.spec.js
@@ -79,7 +79,9 @@ describe('built-in addon catalogue', () => {
       'elephant.calendar',
       'elephant.sites',
       'elephant.ai',
-      'elephant.sync'
+      'elephant.sync',
+      'elephant.code-execution',
+      'elephant.excalidraw'
     ])
     expect(builtinAddons.filter((addon) => addon.manifest.defaultEnabled).map((addon) => addon.manifest.id))
       .toEqual(['elephant.addon-packs'])

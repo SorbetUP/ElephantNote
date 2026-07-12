57e68e7d4c970df9cf5d5cc86d37e6d980c1bc12
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-12 15:30:30 +0200

    Extract quick insert icon library

---
+++ Elephant/frontend/src/muya/lib/ui/quickInsert/icons.js
@@ -0,0 +1,50 @@
+import paragraphIcon from '../../assets/pngicon/paragraph/2.png'
+import htmlIcon from '../../assets/pngicon/html/2.png'
+import hrIcon from '../../assets/pngicon/horizontal_line/2.png'
+import frontMatterIcon from '../../assets/pngicon/front_matter/2.png'
+import header1Icon from '../../assets/pngicon/heading_1/2.png'
+import header2Icon from '../../assets/pngicon/heading_2/2.png'
+import header3Icon from '../../assets/pngicon/heading_3/2.png'
+import header4Icon from '../../assets/pngicon/heading_4/2.png'
+import header5Icon from '../../assets/pngicon/heading_5/2.png'
+import header6Icon from '../../assets/pngicon/heading_6/2.png'
+import newTableIcon from '../../assets/pngicon/new_table/2.png'
+import bulletListIcon from '../../assets/pngicon/bullet_list/2.png'
+import codeIcon from '../../assets/pngicon/code/2.png'
+import quoteIcon from '../../assets/pngicon/quote_block/2.png'
+import todoListIcon from '../../assets/pngicon/todolist/2.png'
+import mathblockIcon from '../../assets/pngicon/math/2.png'
+import orderListIcon from '../../assets/pngicon/order_list/2.png'
+import flowchartIcon from '../../assets/pngicon/flowchart/2.png'
+import sequenceIcon from '../../assets/pngicon/sequence/2.png'
+import plantumlIcon from '../../assets/pngicon/plantuml/2.png'
+import mermaidIcon from '../../assets/pngicon/mermaid/2.png'
+import vegaIcon from '../../assets/pngicon/chart/2.png'
+
+const excalidrawIcon = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiByeD0iMTgiIGZpbGw9IiM2QzYzRkYiLz48cGF0aCBkPSJNMjAgNDRjNy41LTE1LjUgMTUuNS0yMy41IDI0LTI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmYiIHN0cm9rZS13aWR0aD0iNSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+PHBhdGggZD0iTTE4IDQ2bDgtMi02LTYtMiA4eiIgZmlsbD0iI2ZmZiIvPjxwYXRoIGQ9Ik00MiAxOGw0IDQiIHN0cm9rZT0iI2ZmZiIgc3Ryb2tlLXdpZHRoPSI1IiBzdHJva2UtbGluZWNhcD0icm91bmQiLz48Y2lyY2xlIGN4PSIyMiIgY3k9IjIyIiByPSI0IiBmaWxsPSIjZmZmIiBvcGFjaXR5PSIuODUiLz48cGF0aCBkPSJNNDIgNDJoNyIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjQiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgb3BhY2l0eT0iLjg1Ii8+PC9zdmc+'
+
+export const quickInsertIcons = Object.freeze({
+  paragraph: paragraphIcon,
+  html: htmlIcon,
+  hr: hrIcon,
+  frontMatter: frontMatterIcon,
+  header1: header1Icon,
+  header2: header2Icon,
+  header3: header3Icon,
+  header4: header4Icon,
+  header5: header5Icon,
+  header6: header6Icon,
+  table: newTableIcon,
+  bulletList: bulletListIcon,
+  code: codeIcon,
+  quote: quoteIcon,
+  todoList: todoListIcon,
+  mathblock: mathblockIcon,
+  orderList: orderListIcon,
+  flowchart: flowchartIcon,
+  sequence: sequenceIcon,
+  plantuml: plantumlIcon,
+  mermaid: mermaidIcon,
+  vega: vegaIcon,
+  excalidraw: excalidrawIcon
+})

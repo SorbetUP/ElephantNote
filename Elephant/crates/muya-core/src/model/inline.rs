25d73ea5c3fdf5d82bbc26de3b468b569011e549
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-13 00:10:57 +0200

    refactor(muya): model inline constructions

---
+++ Elephant/crates/muya-core/src/model/inline.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum InlineKind {
+  Text { value: String },
+  Emphasis,
+  Strong,
+  Strike,
+  CodeSpan { code: String },
+  Link { destination: String, title: Option<String> },
+  Image { source: String, title: Option<String>, alt: String },
+  AutoLink { destination: String },
+  InlineHtml { raw: String },
+  InlineMath { source: String },
+  Emoji { shortcode: String, value: String },
+  Superscript,
+  Subscript,
+  FootnoteReference { label: String },
+  SoftBreak,
+  HardBreak,
+}

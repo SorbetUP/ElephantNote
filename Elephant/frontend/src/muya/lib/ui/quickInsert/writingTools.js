4770fc6f63ca62e1fddb864968750deafffa63c8
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-12 15:30:50 +0200

    Extract quick insert writing tools library

---
+++ Elephant/frontend/src/muya/lib/ui/quickInsert/writingTools.js
@@ -0,0 +1,87 @@
+import { quickInsertIcons as icons } from './icons'
+
+export const createWritingTools = () => [{
+  title: 'Heading 2',
+  subTitle: '## Heading',
+  label: 'elephant-command heading-2',
+  shortCut: '',
+  icon: icons.header2
+}, {
+  title: 'Bold',
+  subTitle: '**bold**',
+  label: 'elephant-command bold',
+  shortCut: '',
+  icon: icons.paragraph
+}, {
+  title: 'Italic',
+  subTitle: '*italic*',
+  label: 'elephant-command italic',
+  shortCut: '',
+  icon: icons.paragraph
+}, {
+  title: 'Strikethrough',
+  subTitle: '~~text~~',
+  label: 'elephant-command strike',
+  shortCut: '',
+  icon: icons.paragraph
+}, {
+  title: 'Link',
+  subTitle: '[text](url)',
+  label: 'elephant-command link',
+  shortCut: '',
+  icon: icons.paragraph
+}, {
+  title: 'Bullet List',
+  subTitle: '- item',
+  label: 'elephant-command bullets',
+  shortCut: '',
+  icon: icons.bulletList
+}, {
+  title: 'Numbered List',
+  subTitle: '1. item',
+  label: 'elephant-command numbers',
+  shortCut: '',
+  icon: icons.orderList
+}, {
+  title: 'Task List',
+  subTitle: '- [ ] task',
+  label: 'elephant-command tasks',
+  shortCut: '',
+  icon: icons.todoList
+}, {
+  title: 'Inline Code',
+  subTitle: '`code`',
+  label: 'elephant-command code',
+  shortCut: '',
+  icon: icons.code
+}, {
+  title: 'Quote',
+  subTitle: '> quote',
+  label: 'elephant-command quote',
+  shortCut: '',
+  icon: icons.quote
+}, {
+  title: 'Table',
+  subTitle: '| column |',
+  label: 'elephant-command table',
+  shortCut: '',
+  icon: icons.table
+}, {
+  title: 'Image',
+  subTitle: 'Insert image',
+  label: 'elephant-command image',
+  shortCut: '',
+  icon: icons.paragraph
+}, {
+  title: 'Excalidraw',
+  subTitle: 'Insert drawing',
+  label: 'elephant-command excalidraw',
+  shortCut: '',
+  icon: icons.excalidraw
+}, {
+  title: 'Horizontal Rule',
+  subTitle: '---',
+  label: 'elephant-command horizontal-rule',
+  shortCut: '',
+  icon: icons.hr
+}]

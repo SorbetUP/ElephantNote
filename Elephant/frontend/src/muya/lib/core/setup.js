6c2cb9cde004b40e8d93d2eecc00a14ef58a1d96
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-13 05:04:37 +0200

    Wire isolated Rust editor runtime into Muya setup

--- Elephant/frontend/src/muya/lib/core/setup.js
+++ Elephant/frontend/src/muya/lib/core/setup.js
@@ -7,7 +7,7 @@ import DragDrop from '../eventHandler/dragDrop'
 import Resize from '../eventHandler/resize'
 import ClickEvent from '../eventHandler/clickEvent'
 import { MUYA_DEFAULT_OPTION } from '../config'
-import { MuyaRustBridge } from '../rust/bridge'
+import { initializeExperimentalRustRuntime } from '../rust/runtime'
 import ToolTip from '../ui/tooltip'
 import I18nCSS from '../utils/i18nCSS'
 import { getContainer } from './container'
@@ -40,6 +40,7 @@ const installDispatchers = (muya) => {
 const initializeExperimentalRustEditor = (muya) => {
   const config = muya.options.experimentalRustEditor
   muya.rustEditorBridge = null
+  muya.rustEditorRuntime = null
   muya.rustEditorReady = Promise.resolve(null)
   if (!config) return
 
@@ -48,33 +49,17 @@ const initializeExperimentalRustEditor = (muya) => {
     muya.eventCenter.dispatch('rustEditorError', { error })
   }
 
-  muya.rustEditorReady = Promise.resolve()
-    .then(() => config.factory(muya.markdown))
-    .then((engine) => {
-      let bridge
-      try {
-        bridge = new MuyaRustBridge(engine, {
-          applyPatches: config.applyPatches,
-          applySnapshot: config.applySnapshot,
-          onSelection: config.onSelection,
-          onError: reportError
-        })
-      } catch (error) {
-        reportError(error)
-        return null
-      }
-
-      return bridge
-        .snapshot()
-        .then(() => {
-          muya.rustEditorBridge = bridge
-          muya.eventCenter.dispatch('rustEditorReady', {
-            revision: bridge.revision,
-            shadow: !config.applyPatches
-          })
-          return bridge
-        })
-        .catch(() => null)
+  muya.rustEditorReady = initializeExperimentalRustRuntime(muya, config, reportError)
+    .then((runtime) => {
+      muya.rustEditorRuntime = runtime
+      muya.rustEditorBridge = runtime.bridge
+      muya.eventCenter.dispatch('rustEditorReady', {
+        revision: runtime.bridge.revision,
+        shadow: runtime.shadow,
+        rendered: Boolean(runtime.renderer),
+        input: Boolean(runtime.inputController)
+      })
+      return runtime.bridge
     })
     .catch((error) => {
       reportError(error)

b430651d91f38af6730702270fd457cc3867c625
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-13 04:16:10 +0200

    Initialize optional Muya Rust shadow bridge

--- Elephant/frontend/src/muya/lib/core/setup.js
+++ Elephant/frontend/src/muya/lib/core/setup.js
@@ -7,6 +7,7 @@ import DragDrop from '../eventHandler/dragDrop'
 import Resize from '../eventHandler/resize'
 import ClickEvent from '../eventHandler/clickEvent'
 import { MUYA_DEFAULT_OPTION } from '../config'
+import { MuyaRustBridge } from '../rust/bridge'
 import ToolTip from '../ui/tooltip'
 import I18nCSS from '../utils/i18nCSS'
 import { getContainer } from './container'
@@ -36,6 +37,51 @@ const installDispatchers = (muya) => {
   }
 }
 
+const initializeExperimentalRustEditor = (muya) => {
+  const config = muya.options.experimentalRustEditor
+  muya.rustEditorBridge = null
+  muya.rustEditorReady = Promise.resolve(null)
+  if (!config) return
+
+  const reportError = (error) => {
+    config.onError?.(error)
+    muya.eventCenter.dispatch('rustEditorError', { error })
+  }
+
+  muya.rustEditorReady = Promise.resolve()
+    .then(() => config.factory(muya.markdown))
+    .then((engine) => {
+      let bridge
+      try {
+        bridge = new MuyaRustBridge(engine, {
+          applyPatches: config.applyPatches,
+          applySnapshot: config.applySnapshot,
+          onSelection: config.onSelection,
+          onError: reportError
+        })
+      } catch (error) {
+        reportError(error)
+        return null
+      }
+
+      return bridge
+        .snapshot()
+        .then(() => {
+          muya.rustEditorBridge = bridge
+          muya.eventCenter.dispatch('rustEditorReady', {
+            revision: bridge.revision,
+            shadow: !config.applyPatches
+          })
+          return bridge
+        })
+        .catch(() => null)
+    })
+    .catch((error) => {
+      reportError(error)
+      return null
+    })
+}
+
 export const initializeMuya = (muya, MuyaClass, container, options) => {
   muya.options = Object.assign({}, MUYA_DEFAULT_OPTION, options)
   const { markdown } = muya.options
@@ -59,4 +105,5 @@ export const initializeMuya = (muya, MuyaClass, container, options) => {
   muya.i18nCSS = new I18nCSS(muya.options.t, muya.options.quickInsertTrigger)
   installDispatchers(muya)
   muya.init()
+  initializeExperimentalRustEditor(muya)
 }

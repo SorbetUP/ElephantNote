6558b309d2f5736a9fb65ea2a3f97038ead172d2
SorbetUP <155577111+SorbetUP@users.noreply.github.com>
2026-07-13 13:55:42 +0200

    Characterize Muya nested-list container boundaries

--- tests/app/unit/specs/muya/rustDifferentialListTraces.spec.js
+++ tests/app/unit/specs/muya/rustDifferentialListTraces.spec.js
@@ -8,6 +8,36 @@ import {
   setJsSelectionByText
 } from './rustDifferentialHarness'
 
+const describeJsCursor = (muya, label) => {
+  const { contentState } = muya
+  let block = contentState.getBlock(contentState.cursor.start.key)
+  const path = []
+  while (block) {
+    path.push({
+      key: block.key,
+      type: block.type,
+      functionType: block.functionType,
+      listType: block.listType,
+      text: block.text,
+      parent: block.parent,
+      preSibling: block.preSibling,
+      nextSibling: block.nextSibling,
+      children: block.children?.length ?? 0
+    })
+    block = block.parent ? contentState.getBlock(block.parent) : null
+  }
+  console.log(
+    '[muya-list-path]',
+    JSON.stringify({
+      label,
+      cursor: contentState.cursor,
+      indentable: contentState.isIndentableListItem(),
+      unindentable: contentState.isUnindentableListItem(),
+      path
+    })
+  )
+}
+
 const describeBundled = bundled ? describe : describe.skip
 const traces = [
   {
@@ -29,6 +59,7 @@ const traces = [
     expected: '- parent\n  - first\n  - second\n',
     runJs: async (muya) => {
       setJsSelectionByText(muya, 'second', 0)
+      describeJsCursor(muya, 'indent-second')
       muya.contentState.tabHandler(fakeKeyEvent())
     },
     runRust: (rust) => {
@@ -55,6 +86,7 @@ const traces = [
     expected: '- parent\n  - first\n- second\n',
     runJs: async (muya) => {
       setJsSelectionByText(muya, 'second', 0)
+      describeJsCursor(muya, 'outdent-second')
       muya.contentState.tabHandler(fakeKeyEvent({ shiftKey: true }))
     },
     runRust: (rust) => {
